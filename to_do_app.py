0f109594af6a9a2d4fca6dfb0089cd1b0f640db1
baohuy2209 <vvnam965@gmail.com>
2025-01-28 21:35:36 +0700

    Last commit to-do-app

--- to_do_app.py
+++ to_do_app.py
@@ -5,7 +5,7 @@ class ListItem:
 	isComplete = False 
 	title = ''
 	def __init__(self, title): 
-		listItem.append('test')
+		listItem.append(self)
 		self.title = title 
 
 	def set_as_complete(self): 
@@ -13,10 +13,10 @@ class ListItem:
 
 	def get_listITem(self): 
 		return self
-	def update():
-		pass
-	def delete(): 
-		pass 
+	def update(self, index, title):
+		listItem[index].title = title 
+	def delete(self, index): 
+		del listItem[index]
 listItem = ListItem("wash the dishes")
 listItem.set_as_complete()
 print(listItem.get_listITem())
