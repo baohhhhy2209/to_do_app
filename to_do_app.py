edb9d8b4f4105605727c59f0cd0287d85aa2e1ee
baohuy2209 <vvnam965@gmail.com>
2025-01-28 20:53:02 +0700

    to do app using python

---
+++ to_do_app.py
@@ -0,0 +1,22 @@
+
+listItems = list()
+class ListItem:
+
+	isComplete = False 
+	title = ''
+	def __init__(self, title): 
+		listItem.append('test')
+		self.title = title 
+
+	def set_as_complete(self): 
+		self.isComplete = True 
+
+	def get_listITem(self): 
+		return self
+	def update():
+		pass
+	def delete(): 
+		pass 
+listItem = ListItem("wash the dishes")
+listItem.set_as_complete()
+print(listItem.get_listITem())
